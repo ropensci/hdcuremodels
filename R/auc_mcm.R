cfe7d8e9d48436165eba2c2d4d7c73c15dcc0da7
kelliejarcher <archer.43@osu.edu>
2024-10-01 13:59:58 -0400

    Modifications prior to ROpenSci review

---
+++ R/auc_mcm.R
@@ -0,0 +1,179 @@
+#' AUC for cure prediction using mean score imputation
+#'
+#' @description
+#' This function calculates the AUC for cure prediction using the mean score
+#' imputation (MSI) method proposed by Asano et al.
+#'
+#' @param object a \code{mixturecure} object resulting from \code{curegmifs},
+#' \code{cureem}, \code{cv_curegmifs}, \code{cv_cureem}.
+#' @param newdata an optional data.frame that minimally includes the incidence
+#' and/or latency variables to use for predicting the response. If omitted, the
+#' training data are used.
+#' @param cure_cutoff cutoff value for cure, used to produce a proxy for the
+#' unobserved cure status; default is 5.
+#' @param model_select for models fit using \code{curegmifs} or \code{cureem}
+#' any step along the solution path can be selected. The default is
+#' \code{model_select = "AIC"} which calculates the predicted values using
+#' the coefficients from the model having the lowest AIC. Other options are
+#' \code{model_select = "mAIC"} for the modified AIC,
+#' \code{model_select = "cAIC"} for the corrected AIC,
+#' \code{model_select = "BIC"}, \code{model_select = "mBIC"}
+#' for the modified BIC, \code{model_select = "EBIC"} for the extended BIC,
+#' \code{model_select = "logLik"} for the step that maximizes the
+#' log-likelihood, or any numeric value from the solution path.
+#' This option has no effect for objects fit using \code{cv_curegmifs} or
+#' \code{cv_cureem}.
+#'
+#' @return Returns the AUC value for cure prediction using the mean score
+#' imputation (MSI) method.
+#' @export
+#'
+#' @references Asano, J., Hirakawa, H., Hamada, C. (2014) Assessing the
+#' prediction accuracy of cure in the Cox proportional hazards cure model:
+#' an application to breast cancer data. \emph{Pharmaceutical Statistics},
+#' \bold{13}:357--363.
+#'
+#' @seealso \code{\link{concordance_mcm}}
+#'
+#' @keywords univar
+#' @examples
+#' library(survival)
+#' set.seed(1234)
+#' temp <- generate_cure_data(n = 100, j = 10, n_true = 10, a = 1.8)
+#' training <- temp$training
+#' testing <- temp$testing
+#' fit <- curegmifs(Surv(Time, Censor) ~ .,
+#'                   data = training, x_latency = training,
+#'                   model = "weibull", thresh = 1e-4, maxit = 2000,
+#'                   epsilon = 0.01, verbose = FALSE)
+#' auc_mcm(fit, model_select = "cAIC")
+#' auc_mcm(fit, newdata = testing)
+auc_mcm <- function(object, newdata, cure_cutoff = 5, model_select = "AIC") {
+  if (!("mixturecure" %in% class(object)))
+    stop("class of object must be mixturecure")
+  no_data <- (missing(newdata) || is.null(newdata))
+  if (no_data) {
+    testing_time <- object[["y"]][, 1]
+    testing_delta <- object[["y"]][, 2]
+    x_p <- object[["x_incidence"]]
+  } else {
+    df <- model.frame(parse(text = object$call, keep.source = FALSE)[[2]],
+                      data = newdata)
+    testing_time <- df[, 1][, 1]
+    testing_delta <- df[, 1][, 2]
+    x_p <- as.matrix(model.frame(as.formula(paste(" ~ ", paste(
+              colnames(object$x_incidence), collapse = "+"))), newdata))
+  }
+  if (!is.null(x_p) && identical(x_p, object$x_incidence)) {
+    if (object$scale) {
+      sd <- apply(x_p, 2, sd)
+      for (i in seq_len(dim(x_p)[2])) {
+        if (sd[i] == 0) {
+          x_p[, i] <- scale(x_p[, i], center = TRUE,
+                            scale = FALSE)
+        } else {
+          x_p[, i] <- scale(x_p[, i], center = TRUE,
+                            scale = TRUE)
+        }
+      }
+    }
+  } else if (!is.null(x_p) && object$scale) {
+    newx <- rbind(object$x_incidence, x_p)
+    sd <- apply(newx, 2, sd)
+    for (i in seq_len(dim(newx)[2])) {
+      if (sd[i] == 0) {
+        newx[, i] <- scale(newx[, i], center = TRUE,
+                           scale = FALSE)
+      } else {
+        newx[, i] <- scale(newx[, i], center = TRUE,
+                           scale = TRUE)
+      }
+    }
+    x_p <- as.matrix(newx[-(1:dim(object$x_incidence)[1]), , drop = FALSE])
+  }
+  testing_n <- length(testing_time)
+  v <- rep(0, testing_n)
+  y <- rep(999, testing_n)
+  y[testing_time > cure_cutoff] <- 0
+  y[testing_time <= cure_cutoff & testing_delta == 1] <- 1
+  v[y < 2] <- 1
+  # Extract b_p_hat and intercept at model_select
+  if (!object$cv) {
+    if (is.character(model_select)) {
+      model_select <- c("AIC", "BIC", "logLik", "cAIC", "mAIC",
+                        "mBIC", "EBIC")[pmatch(model_select, c("AIC", "BIC",
+                        "logLik", "cAIC", "mAIC", "mBIC", "EBIC"))]
+      if (any(!model_select %in% c("AIC", "BIC", "logLik", "cAIC", "mAIC",
+                "mBIC", "EBIC")))
+        stop("model_select must be either 'AIC', 'BIC', 'logLik', 'cAIC',
+             'mAIC', 'mBIC', or 'EBIC' ")
+      if (object$method == "EM") {
+        logLik <- object$logLik_inc + object$logLik_lat
+      } else {
+        logLik <- object$logLik
+      }
+      if (!is.null(object$x_incidence)) {
+        vars_inc <- apply(object$b_path, 1, function(x) sum(x !=  0))
+      } else {
+        vars_inc <- 0
+      }
+      if (!is.null(object$x_latency)) {
+        vars_lat <- apply(object$beta_path, 1, function(x) sum(x != 0))
+      } else {
+        vars_lat <- 0
+      }
+      if (object$model == "weibull") {
+        df <- vars_inc + vars_lat + 3
+      } else if (object$model == "exponential") {
+        df <- vars_inc + vars_lat + 2
+      } else if (object$model == "cox") {
+        df <- vars_inc + vars_lat + 1
+      }
+      p <- dim(object$x_incidence)[2] + dim(object$x_latency)[2]
+      AIC <- 2 * df - 2 * logLik
+      cAIC <- AIC + (2 * df * (df + 1)) / (length(object$y) - df - 1)
+      mAIC <- (2 + 2 * log(p/.5)) * df - 2 * logLik
+      BIC <- df * (log(length(object$y))) -  2 * logLik
+      mBIC <- df * (log(length(object$y)) + 2 * log(p / 4)) - 2 * logLik
+      EBIC <- log(length(object$y)) * df + 2 * (1 - .5) * log(choose(p, df)) -
+        2 * logLik
+      if (model_select == "AIC") {
+        model_select <- which.min(AIC)
+      } else if (model_select == "BIC") {
+        model_select <- which.min(BIC)
+      } else if (model_select == "mAIC") {
+        model_select <- which.min(mAIC)
+      } else if (model_select == "mBIC") {
+        model_select <- which.min(mBIC)
+      } else if (model_select == "EBIC") {
+        model_select <- which.min(EBIC)
+      } else if (model_select == "cAIC") {
+        model_select <- which.min(cAIC)
+      } else if (model_select == "logLik") {
+        model_select <- which.max(logLik)
+      }
+    }
+    itct_hat <- object$b0_path[model_select]
+    b_p_hat <- object$b_path[model_select, , drop = FALSE]
+
+  } else {
+    itct_hat <- object$b0
+    b_p_hat <- matrix(object$b, ncol = 1)
+  }
+  if (all(b_p_hat == 0)) {
+    xb <- rep(itct_hat, testing_n)
+  }  else {
+    xb <- itct_hat + x_p[, b_p_hat != 0, drop = FALSE] %*% b_p_hat[b_p_hat != 0]
+  }
+  p_hat <- 1 / (1 + exp(-xb))
+  temp <- v * y + (1 - v) * p_hat
+  temp1 <- temp[order(p_hat, decreasing = TRUE)]
+  temp_f <- v * (1 - y) + (1 - v) * (1 - p_hat)
+  temp1f <- temp_f[order(p_hat, decreasing = TRUE)]
+  TPR <- c(0, cumsum(temp1) / cumsum(temp1)[testing_n])
+  FPR <- c(0, cumsum(temp1f) / cumsum(temp1f)[testing_n])
+  height <- (TPR[-1] + TPR[-length(TPR)]) / 2
+  width <- diff(FPR)
+  auc_msi <- sum(height * width)
+  return(auc_msi)
+}
---
+++ README.md
@@ -0,0 +1,150 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# hdcuremodels
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of hdcuremodels is to allow one to fit a penalized mixture cure
+model (MCM) when there is a high-dimensional covariate space, such as
+when high-throughput genomic data are used in modeling time-to-event
+data, when some subjects will not experience the event of interest.
+Conventionally, we call the subset of subjects who are immune to the
+event of interest cured while all other subjects are susceptible to the
+event.
+
+## Installation
+
+You can install the development version of hdcuremodels like so:
+
+``` r
+install.packages("hdcuremodels")
+```
+
+## Example
+
+After loading the `hdcuremodels` and `survival` packages, load
+`amltrain` which includes 306 patients diagnosed with acute myeloid
+leukemia (AML) who were cytogenetically normal at diagnosis along with
+time-to-event outcomes: `cryr` is the duration of complete response (in
+years), `relapse.death` is a censoring variable where 1 indicates the
+patient relapsed or died and 0 indicates the patient was alive at last
+follow-up. It is of interest to fit a MCM using the expression for 320
+transcripts measured using RNA-sequencing as predictor variables.
+
+``` r
+library(hdcuremodels)
+library(survival)
+```
+
+``` r
+data(amltrain)
+```
+
+We can inspect the Kaplan-Meier survival curve to determine whether a
+cure fraction seems to be present.
+
+``` r
+km_train <- survfit(Surv(cryr, relapse.death) ~ 1, data = amltrain)
+```
+
+<img src="man/figures/README-unnamed-chunk-2-1.png" width="100%" />
+
+As can be seen from the Kaplan-Meier plot, there is a long-plateau that
+does not drop down to zero. This may indicate the presence of a cured
+fraction. We can test the null hypothesis that the cured fraction is
+zero against the alternative hypothesis that the cured fraction is not
+zero using the `nonzerocure_test` function.
+
+``` r
+nonzerocure_test(km_train)
+#> $proportion_susceptible
+#> [1] 0.7146919
+#> 
+#> $proportion_cured
+#> [1] 0.2853081
+#> 
+#> $p_value
+#> [1] "< 0.001"
+#> 
+#> $time_95_percent_of_events
+#> [1] 5.294299
+```
+
+Given the small p-value we reject the null hypothesis and conclude there
+is a non-zero cure fraction present. We can also extract the cured
+fraction as the Kaplan-Meier estimate beyond the last observed event
+using the `cure_estimate` function.
+
+``` r
+cure_estimate(km_train)
+#> [1] 0.2853081
+```
+
+We now fit a penalized MCM using the E-M algorithm where the penalty
+parameters for incidence and latency, `lambda_inc` and `lambda_lat` were
+previously determined using cross-validation.
+
+``` r
+fitem <- cureem(Surv(cryr, relapse.death) ~ ., data = amltrain, 
+                      x_latency = amltrain, model = "cox", 
+                      lambda_inc = 0.009993, lambda_lat = 0.02655)
+```
+
+Coefficient estimates can be extracted from the fitted model using the
+`coef` for any of these model criteria (“logLik”, “AIC”, “cAIC”, “mAIC”,
+“BIC”, “mBIC”, “EBIC”) or by specifying the step at which the model is
+desired by specifying the `model_select` parameter. For example,
+
+``` r
+coef.cAIC <- coef(fitem, model_select = "cAIC")
+```
+
+Predictions can be extracted at a given step or information criterion
+(“logLik”, “AIC”, “cAIC”, “mAIC”, “BIC”, “mBIC”, “EBIC”) using the
+`predict` function with `model_select` specified.
+
+``` r
+train_predict <- predict(fitem, model_select = "cAIC")
+```
+
+This returns three objects: `p_uncured` is the estimated probability of
+being susceptible ($\hat{p}(\mathbf{x})$), `linear_latency` is
+$\hat{\boldsymbol{\beta}}\mathbf{w}$, while `latency_risk` applies high
+risk and low risk labels using zero as the cutpoint from the
+`linear_latency` vector. Perhaps we want to apply the 0.5 threshold to
+`p_uncured` to create Cured and Susceptible labels.
+
+``` r
+p_group <- ifelse(train_predict$p_uncured < 0.50, "Cured", "Susceptible")
+```
+
+Then we can assess how well our MCM identified patients likely to be
+cured from those likely to be susceptible visually by examining the
+Kaplan-Meier curves.
+
+``` r
+km_cured <- survfit(Surv(cryr, relapse.death) ~ p_group, data = amltrain) 
+```
+
+<img src="man/figures/README-unnamed-chunk-5-1.png" width="100%" />
+
+We can assess how well our MCM identified higher versus lower risk
+patients among those predicted to be susceptible visually by examining
+the Kaplan-Meier curves.
+
+``` r
+km_suscept <- survfit(Surv(cryr, relapse.death) ~ train_predict$latency_risk, data = amltrain, subset = (p_group == "Susceptible")) 
+```
+
+<img src="man/figures/README-unnamed-chunk-6-1.png" width="100%" />
+
+Of course, we expect our model to perform well on our training data. We
+can also assess how well our fitted MCM performs using the independent
+test set `amltest`. In this case we use the `predict` function with
+`newdata` specified.
+
+``` r
+test_predict <- predict(fitem, newdata = amltest, model_select = "cAIC")
+```
